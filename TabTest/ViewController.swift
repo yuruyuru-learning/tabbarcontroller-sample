7468a51863139bedc57be8403ce4e8dfdd2a9966
クワシマ・ユウキ <yukikuwa@kuwashimayuukinoMacBook-Pro.local>
2021-08-14 20:13:16 +0900

    aa2

---
+++ TabTest/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TabTest
+//
+//  Created by クワシマ・ユウキ on 2021/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("1")
+    }
+    
+    
+
+
+}
+
---
+++ TabTest/ViewController2.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController2.swift
+//  TabTest
+//
+//  Created by クワシマ・ユウキ on 2021/08/14.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print("2")
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
