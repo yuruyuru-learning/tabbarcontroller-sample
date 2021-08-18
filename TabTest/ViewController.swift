1bbdfdc5a00e725d7ab5cfc2612c1a56389a3c98
yukiny0811 <yukiny0811@gmail.com>
2021-08-19 03:54:32 +0900

    aaa

--- TabTest/ViewController.swift
+++ TabTest/ViewController.swift
@@ -5,12 +5,40 @@
 //  Created by クワシマ・ユウキ on 2021/08/14.
 //
 
+//クイズ用
+
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UITableViewDelegate, UITableViewDataSource{
+    
+    let saveData = UserDefaults.standard
+    
+    var allTechnique =  ["ノーマル","ダブル","リバース","シングルアクセル","ハーモニック","セミリバース","カウンター","パームスピン","カージオイド","シングルアクセルリバース","フィンガーレスノーマル","フィンガーレスリバース","ハーフフェイクトリバース","ハーフフェイクトノーマル","コンティニュアスシングルアクセルカージオイド"]
+    var checkedTechnique: [String] = []
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return checkedTechnique.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        
+        cell?.textLabel?.text = checkedTechnique[indexPath.row]
+        
+               
+        return cell!
+    }
+    
+    
+    @IBOutlet var table: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        table.delegate = self
+        table.dataSource = self
+        
+        
+        
         
         // Do any additional setup after loading the view.
     }
@@ -19,6 +47,24 @@ class ViewController: UIViewController {
         super.viewDidAppear(animated)
         
         print("1")
+        
+        var checkData: [Bool] = []
+        
+        if saveData.object(forKey: "checkData") != nil {
+         
+            checkData = saveData.object(forKey: "checkData") as! [Bool]
+            
+            checkedTechnique.removeAll()
+            
+            for i in 0..<checkData.count {
+                if (checkData[i] == true) {
+                    checkedTechnique.append(allTechnique[i])
+                }
+            }
+            
+            table.reloadData()
+         
+        }
     }
     
     
--- TabTest/ViewController2.swift
+++ TabTest/ViewController2.swift
@@ -5,12 +5,83 @@
 //  Created by クワシマ・ユウキ on 2021/08/14.
 //
 
+//技全部表示用
+
 import UIKit
 
-class ViewController2: UIViewController {
+class ViewController2: UIViewController , UITableViewDelegate, UITableViewDataSource{
+    
+    var technique: [String] = []
+    var check: [Bool] = []
+    
+    var saveData: UserDefaults = UserDefaults.standard
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        technique.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        
+        cell?.textLabel?.frame = CGRect(x: 0, y: 0, width: tableView.frame.width * 0.8, height: (cell?.frame.height)!)
+
+        cell?.textLabel?.text = technique[indexPath.row]
+        
+        for v in cell!.subviews {
+            if let v = v as? UIButton {
+                v.removeFromSuperview()
+            }
+        }
+               
+        let button = UIButton()
+        
+        button.frame = CGRect(x: tableView.frame.width * 0.8, y: 0, width: tableView.frame.width * 0.2, height: (cell?.frame.height)!)
+        
+        if check[indexPath.row] == true {
+            button.setTitle("✅", for: .normal)
+        } else {
+            button.setTitle("❌", for: .normal)
+        }
+        
+        button.tag = indexPath.row
+        
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+        cell?.addSubview(button)
+               
+        return cell!
+    }
+    
+    @objc func buttonTapped(_ sender: UIButton) {
+        check[sender.tag] = !check[sender.tag]
+        
+        saveData.setValue(check, forKey: "checkData")
+        
+        table.reloadData()
+    }
+    
+    
+    @IBOutlet var table: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        technique = ["ノーマル","ダブル","リバース","シングルアクセル","ハーモニック","セミリバース","カウンター","パームスピン","カージオイド","シングルアクセルリバース","フィンガーレスノーマル","フィンガーレスリバース","ハーフフェイクトリバース","ハーフフェイクトノーマル","コンティニュアスシングルアクセルカージオイド"]
+        
+        check = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]
+        
+        table.delegate = self
+        table.dataSource = self
+        
+        if saveData.object(forKey: "checkData") != nil {
+         
+            check = saveData.object(forKey: "checkData") as! [Bool]
+         
+        } else {
+            saveData.setValue(check, forKey: "checkData")
+        }
+        
+        table.reloadData()
         
         // Do any additional setup after loading the view.
     }
